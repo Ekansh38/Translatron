753d94bbf3b42f4282e6834130f172ec4450c2d1
Ekansh Goenka <goenka.ekansh@gmail.com>
2024-01-08 15:01:56 +0800

    Added form and got data

--- Translatron/translator/views.py
+++ Translatron/translator/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+langs = [
+        "Spanish",
+        "English",
+        "French",
+        "German",
+        "Italian",
+        "Portuguese",
+        "Russian",
+        "Chinese",
+        "Japanese",
+        ]
+
+
+def index(request):
+    if request.method == "POST":
+        source_lang = request.POST["source_lang"]
+        target_lang = request.POST["target_lang"]
+        source_text = request.POST["source_text"]
+        print(source_lang, target_lang, source_text)
+
+    return render(request, "translator/index.html", {
+        "source_lang": None,
+        "target_lang": None,
+        "source_text": None,
+        "target_text": None,
+        "langs": langs,
+        })
